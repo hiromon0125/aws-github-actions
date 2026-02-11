f218a2e2e4c17d7a13630502497e490952f6a9e0
Hiro <hiroto125takeuchi@gmail.com>
2026-02-10 22:19:38 -0500

    fix: bucket in the correct region

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hiro-bucket-swen614"
+    bucket = "hiro-terraform-activity-bucket"
     key    = "activity-prod/terraform.tfstate"
     region = "us-east-1"
   }
